4b98db81d67fba3d6159c84a08ce963eaa78882d
Federico Resnizky <fresnizky@makingsense.com>
2022-07-25 13:46:08 -0300

    Initial Commit

---
+++ App.js
@@ -0,0 +1,252 @@
+import React, {Component} from 'react';
+import {
+  Platform,
+  PixelRatio,
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  TextInput,
+  View,
+  Image,
+  ImageBackground,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import usabilla from 'usabilla-react-native';
+import {bgImage, logo} from './assets/images';
+import {BackHandler} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu',
+});
+
+/// Usabilla Configuration
+// Replace appId with your usabilla app id.
+const appId = '91ce2c24-2150-48e6-9e56-77f8991b15a4';
+// Replace FormId with your usabilla form id.
+const formId = '62cf2d728ec6d724a8511b2f';
+// Replace custom variable with your usabilla custom variable created for targeting specific Campaign..
+const customVars = {Key: 'Value'};
+
+var isFormVisible = false;
+
+export default class App extends Component<{}> {
+  constructor() {
+    super();
+    this.state = {text: ''};
+    this.handleBackButtonClick = this.handleBackButtonClick.bind(this);
+    usabilla.initialize(appId);
+    usabilla.setDataMasking(usabilla.getDefaultDataMasks(), 'X');
+    usabilla.setCustomVariables(customVars);
+    usabilla.setFormDidLoadSuccessfully(reminder => {
+      isFormVisible = true;
+      console.log('successfull loading form: ', reminder);
+    });
+    usabilla.setFormDidFailLoading(reminder =>
+      console.log('Error loading form: ', reminder),
+    );
+    usabilla.setFormDidClose(reminder => {
+      isFormVisible = false;
+      console.log('Form closed: ', reminder);
+    });
+    usabilla.setCampaignDidClose(reminder =>
+      console.log('Campaign closed: ', JSON.stringify(reminder)),
+    );
+  }
+
+  componentDidMount() {
+    BackHandler.addEventListener(
+      'hardwareBackPress',
+      this.handleBackButtonClick,
+    );
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener(
+      'hardwareBackPress',
+      this.handleBackButtonClick,
+    );
+  }
+
+  handleBackButtonClick() {
+    usabilla.onBackPressed();
+    return isFormVisible;
+  }
+
+  resetCampaignData() {
+    usabilla.resetCampaignData(() => {
+      console.log('Campaign data is successfully reset!');
+    });
+  }
+
+  sendEvent(event) {
+    usabilla.sendEvent(event);
+  }
+
+  requestFormWithDefaultScreenshot() {
+    usabilla.loadFeedbackFormWithCurrentViewScreenshot(formId);
+  }
+
+  render() {
+    return (
+      <SafeAreaView>
+        <View style={styles.container}>
+          <ImageBackground source={bgImage} style={styles.backgroundImage}>
+            <View style={styles.top}>
+              <Text style={styles.welcome}>Usabilla</Text>
+              <Text style={styles.welcome}>React Native</Text>
+              <Text style={styles.welcome}>Demo App</Text>
+            </View>
+            <View style={styles.middle}>
+              <View style={styles.showForm}>
+                <TouchableWithoutFeedback
+                  onPress={this.requestFormWithDefaultScreenshot}>
+                  <View style={styles.buttonShowForm}>
+                    <Text style={styles.textShowForm}>SHOW FORM</Text>
+                  </View>
+                </TouchableWithoutFeedback>
+              </View>
+              <View style={styles.event}>
+                <TextInput
+                  style={styles.eventInput}
+                  placeholder="Enter event here"
+                  onChangeText={text => this.setState({text})}></TextInput>
+                <TouchableWithoutFeedback
+                  onPress={() => this.sendEvent(this.state.text)}>
+                  <View style={styles.buttonSend}>
+                    <Text style={styles.textSend}>SEND EVENT</Text>
+                  </View>
+                </TouchableWithoutFeedback>
+              </View>
+              <View style={styles.reset}>
+                <TouchableWithoutFeedback onPress={this.resetCampaignData}>
+                  <View style={styles.buttonReset}>
+                    <Text style={styles.textReset}>RESET</Text>
+                  </View>
+                </TouchableWithoutFeedback>
+              </View>
+            </View>
+            <View style={styles.bottom}>
+              <Image style={styles.footerImage} source={logo}></Image>
+            </View>
+          </ImageBackground>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+  },
+  backgroundImage: {
+    width: '100%',
+    height: '100%',
+  },
+  top: {
+    marginTop: PixelRatio.roundToNearestPixel(20),
+    marginLeft: PixelRatio.roundToNearestPixel(40),
+    textAlign: 'left',
+  },
+  welcome: {
+    fontFamily: 'MiloOT-Bold',
+    fontSize: 55 / PixelRatio.getFontScale(),
+    letterSpacing: PixelRatio.roundToNearestPixel(0),
+    color: '#00A5C9',
+  },
+  middle: {
+    marginTop: PixelRatio.roundToNearestPixel(115),
+    marginLeft: PixelRatio.roundToNearestPixel(40),
+    marginRight: PixelRatio.roundToNearestPixel(40),
+    flexDirection: 'column',
+  },
+  showForm: {
+    marginTop: PixelRatio.roundToNearestPixel(0),
+  },
+  buttonShowForm: {
+    backgroundColor: '#00A5C9',
+    borderWidth: PixelRatio.roundToNearestPixel(1),
+    borderRadius: PixelRatio.roundToNearestPixel(2),
+    borderColor: '#ffffff',
+    borderStyle: 'solid',
+    width: 'auto',
+  },
+  textShowForm: {
+    textAlign: 'center',
+    color: '#ffffff',
+    fontFamily: 'MiloOT-Medi',
+    fontSize: 19 / PixelRatio.getFontScale(),
+    letterSpacing: PixelRatio.roundToNearestPixel(0),
+    lineHeight: PixelRatio.roundToNearestPixel(19),
+    padding: PixelRatio.roundToNearestPixel(14),
+  },
+  event: {
+    marginTop: PixelRatio.roundToNearestPixel(40),
+    flexDirection: 'row',
+    display: 'flex',
+    height: PixelRatio.roundToNearestPixel(48),
+  },
+  eventInput: {
+    padding: PixelRatio.roundToNearestPixel(10),
+    marginRight: PixelRatio.roundToNearestPixel(16),
+    borderWidth: PixelRatio.roundToNearestPixel(2),
+    fontFamily: 'MiloOT-Text',
+    fontSize: 19 / PixelRatio.getFontScale(),
+    letterSpacing: PixelRatio.roundToNearestPixel(0),
+    lineHeight: PixelRatio.roundToNearestPixel(19),
+    borderRadius: PixelRatio.roundToNearestPixel(2),
+    borderWidth: PixelRatio.roundToNearestPixel(2),
+    borderStyle: 'solid',
+    borderColor: '#C8D2DA',
+    backgroundColor: '#FFFFFF',
+    justifyContent: 'center',
+    flex: 0.5,
+  },
+  buttonSend: {
+    backgroundColor: '#ffffff',
+    borderWidth: PixelRatio.roundToNearestPixel(1),
+    borderRadius: PixelRatio.roundToNearestPixel(2),
+    borderColor: '#00A5C9',
+    flex: 0.5,
+  },
+  textSend: {
+    textAlign: 'center',
+    color: '#00A5C9',
+    fontFamily: 'MiloOT-Medi',
+    fontSize: 19 / PixelRatio.getFontScale(),
+    letterSpacing: PixelRatio.roundToNearestPixel(0),
+    padding: PixelRatio.roundToNearestPixel(12),
+  },
+  reset: {
+    marginTop: PixelRatio.roundToNearestPixel(16),
+  },
+  buttonReset: {
+    backgroundColor: '#ffffff',
+    borderWidth: PixelRatio.roundToNearestPixel(1),
+    borderRadius: PixelRatio.roundToNearestPixel(2),
+    borderColor: '#00A5C9',
+    width: 'auto',
+  },
+  textReset: {
+    textAlign: 'center',
+    color: '#00A5C9',
+    fontFamily: 'MiloOT-Medi',
+    fontSize: 19 / PixelRatio.getFontScale(),
+    letterSpacing: 0,
+    lineHeight: 19,
+    padding: 14,
+  },
+  bottom: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: PixelRatio.roundToNearestPixel(0),
+  },
+  footerImage: {
+    height: PixelRatio.roundToNearestPixel(50),
+    width: PixelRatio.roundToNearestPixel(83),
+  },
+});
